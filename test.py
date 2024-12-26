6c625984ea0cde95ee32283613873c7a88f0b109
EL BACHAR WALID <walidelbachar1@gmail.com>
2024-12-26 01:04:54 +0100

    jsp

--- test.py
+++ test.py
@@ -1,11 +1,7 @@
-from tensorflow.python.client import device_lib
-print(device_lib.list_local_devices())
+import tensorflow as tf
 
-from tensorflow.python.client import device_lib
-
-# List local devices
-local_devices = device_lib.list_local_devices()
-
-# Print local devices
-for device in local_devices:
-    print(device.name, device.device_type, device.memory_limit)
+# Vérifiez si un GPU est disponible
+if tf.config.list_physical_devices('GPU'):
+    print("GPU est disponible.")
+else:
+    print("GPU n'est pas disponible.")
